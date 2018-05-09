c447632fc3976b8e8fffefdd53698b5da1b74bd2
Maxim Semenov <maxim.semenov@ostrovok.ru>
2018-05-09 13:52:17 +0300

    Init

---
+++ gggg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gggg
+//
+//  Created by Maxim Semenov on 09/05/2018.
+//  Copyright © 2018 SolidMindValley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gggg/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gggg
+//
+//  Created by Maxim Semenov on 09/05/2018.
+//  Copyright © 2018 SolidMindValley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
